f38243053889f4202f6cdfd2c4d29196322a6f1d
Aloey <macintoshvietnam@Aloeey.local>
2022-04-25 08:43:38 +0700

    Second commit styling center #root

--- src/App.js
+++ src/App.js
@@ -4,28 +4,39 @@ import Controller from "./components/Controller";
 import { MusicPlayerProvider } from "./contexts/MusicPlayerContext";
 import Stack from "@mui/material/Stack";
 import Banner from "./components/Banner";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 import "./Globalcss.css";
 const App = () => {
   return (
-    <MusicPlayerProvider>
-      <Stack
-        className="container"
-        m={8}
-        direction="column"
-        alignItems="center"
-        justifyContent="space-between"
-        width="300px"
-        height="500px"
-        sx={{
-          backgroundColor: "#D3BBDD",
-          borderRadius: "70px",
-        }}
-      >
-        <Banner />
-        <TrackList />
-        <Controller />
-      </Stack>
-    </MusicPlayerProvider>
+    <>
+      <MusicPlayerProvider>
+        <Stack
+          className="container"
+          m={8}
+          direction="column"
+          alignItems="center"
+          justifyContent="space-between"
+          width="300px"
+          height="500px"
+          sx={{
+            backgroundColor: "#D3BBDD",
+            borderRadius: "70px",
+          }}
+        >
+          <Banner />
+          <TrackList />
+          <Controller />
+        </Stack>
+      </MusicPlayerProvider>
+      <Typography variant="body2" align="center" p={1} sx={{ color: "beige" }}>
+        Copyright ©
+        <Link color="inherit" href="https://www.coderschool.vn">
+          CoderSchool
+        </Link>
+        {new Date().getFullYear()}
+      </Typography>
+    </>
   );
 };
 
